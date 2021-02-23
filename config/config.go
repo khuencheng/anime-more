d7bbd46c31b5336cadee3351ea7763a18dbb683a
Ceres Cheng <chengzhoukun@gmail.com>
2021-02-24 00:48:23 +0800

    embed config file

--- config/config.go
+++ config/config.go
@@ -1,22 +1,26 @@
 package config
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/spf13/viper"
 	"log"
+	_ "embed"
 )
 
 var config *viper.Viper
 
+//go:embed conf.toml
+var confFile []byte
+
 func Init() {
 	config = viper.New()
 	config.SetConfigType("toml")
-	config.SetConfigName("dev")
-	config.AddConfigPath("../config/")
-	config.AddConfigPath("config/")
-	err := config.ReadInConfig()
+	err := config.ReadConfig(bytes.NewBuffer(confFile))
 	if err != nil {
 		log.Fatal("error on parsing configuration file")
 	}
+	fmt.Println(config.AllKeys())
 }
 
 func GetConfig() *viper.Viper {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module anime-more
 
-go 1.15
+go 1.16
 
 require (
 	github.com/anaskhan96/soup v1.2.4
